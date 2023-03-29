5240bd4178b8dd9ec9283b70e58297777e893912
Debora Faria <deeborafaria@gmail.com>
2023-03-29 19:09:40 -0300

    Update pages

--- src/app.js
+++ src/app.js
@@ -1,18 +1,7 @@
-/*
-Exemplo simples de projeto com uma estrutura
-de diretórios organizada.
-Autor: Fabrício G. M. de Carvalho, Ph.D
-*/
-
 /* importando o express */
 const express = require('express')
 const app = express();
 const port = 5000;
-
-/* importando o modelo */
-const modelo = require('./models/modelos');
-var Projeto = modelo.Projeto; //Vinculação de tipo
-
 
 /* Configurando a template engine. */
 app.set('view engine', 'ejs');
@@ -21,32 +10,18 @@ app.set('views', './views');
 /* Configurando o diretório que serve arquivos estáticos.*/
 app.use(express.static('public'));
 
-app.get('/', listProjectHandler);
-
-app.get('/login', loginHandler);
+app.get('/', loginHandler);
 
 app.get('/cadastro', cadastroHandler);
 
-
+app.get('/buscar', cadastroHandler);
 
 app.listen(port, listenHandler);
 
-function listProjectHandler(req, res){
-    /* Os dados a seguir, em uma aplicação real, deveriam vir de um BD */
-    let projeto_1 = new Projeto("software","JavaScript", 2020, 2023); 
-    let projeto_2 = new Projeto("social","",2020,2021);
-    let projeto_3 = new Projeto("software","HTML", 2021, 2021);    
-    let projetos = [];
-    projetos.push(projeto_1);
-    projetos.push(projeto_2);
-    projetos.push(projeto_3);
-    res.render('listar_projetos.ejs',{lista_projetos: projetos});    
-}
 
 function listenHandler(){
-    console.log(`Escutando na porta ${port}!`);
+    console.log(`Executando na porta ${port}! Segue o link http://localhost:5000/`);
 }
-
 
 
 function loginHandler(req, res){
@@ -61,3 +36,8 @@ function cadastroHandler(req, res){
     res.render('cadastro.ejs');    
 }
 
+function buscarHandler(req, res){
+    /* Os dados a seguir, em uma aplicação real, deveriam vir de um BD */
+   
+    res.render('buscar.ejs');    
+}
